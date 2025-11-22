2e999da8005b778f6fb603aad7289f3dc7ac3165
SinghArmaandeep <asingh29_be24@thapar.edu>
2025-11-23 02:42:18 +0530

    Update a6.cpp

--- a6.cpp
+++ a6.cpp
@@ -375,3 +375,189 @@ int main() {
         }
     }
 }
+
+//4. Write a program to check if a doubly linked list of characters is palindrome or not.
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    char data;
+    Node* next;
+    Node* prev;
+    Node(char c) {
+        data = c;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class dll{
+    Node* head=nullptr;
+    Node* temp=nullptr;
+    
+public:
+    void insert(char val){
+        Node* newnode= new Node(val);
+        if(head==nullptr){
+            head=newnode;
+            temp=newnode;
+            return;
+        }
+        else{
+            temp->next=newnode;
+            newnode->prev=temp;
+            temp=newnode;
+        }
+    
+    }
+    void display(){
+        Node* curr=head;
+        while(curr){
+            cout<<curr->data<<"  ";
+            curr=curr->next;
+        }
+        cout<<endl;
+    }
+    void size(){
+        int len=0;
+        Node* curr=head;
+        while(curr){
+            len++;
+            curr=curr->next;
+        }
+        cout<<"Size of list is "<<len<<endl;
+    }
+   void palindrome(){
+        Node* start = head;
+        Node* end = temp;
+        if (head == nullptr) {
+            cout << "List is empty (considered a palindrome)." << endl;
+            return;
+        }
+        bool flag = true;
+        while(start != end && start->prev != end) { 
+            if(start->data != end->data){
+                flag = false;
+                break;
+            }
+            start = start->next;
+            end = end->prev;
+        }
+        
+        if(flag){
+            cout << "It is a palindrome" << endl;
+        }
+        else{
+            cout << "Not a palindrome" << endl;
+        }
+    }
+};
+
+int main() {
+    
+    dll l;
+    l.insert('l');
+    l.insert('e');
+    l.insert('v');
+    l.insert('e');
+    //l.insert('l');
+    l.display();
+    l.palindrome();
+    
+    return 0;
+}
+
+=================================================================================================================================================
+                                                                 ADDITIONAL QUESTIONS
+=================================================================================================================================================
+
+1. Given a Circular linked list. The task is split into two Circular Linked lists. If there are an
+odd number of nodes in the given circular linked list then out of the resulting two halved
+lists, the first list should have one node more than the second list.
+https://www.geeksforgeeks.org/split-a-circular-linked-list-into-two-halves/
+Input: 10->4->9
+Output: 10->4 , 9
+
+
+// C++ Program to split a circular linked list
+// into two halves 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node { 
+    public:
+    int data; 
+    Node *next; 
+    Node (int new_value){
+        data = new_value;
+        next = nullptr;
+    }
+}; 
+
+pair<Node*, Node*> splitList(Node *head) { 
+    Node *slow = head; 
+    Node *fast = head; 
+    
+    if(head == nullptr) 
+        return {nullptr, nullptr}; 
+        
+    while(fast->next != head && 
+          fast->next->next != head) { 
+        fast = fast->next->next; 
+        slow = slow->next; 
+    } 
+    
+    // If there are even elements in list
+    // then move fast
+    if(fast->next->next == head) 
+        fast = fast->next; 
+        
+    // Set the head pointer of first half
+    Node* head1 = head; 
+        
+    // Set the head pointer of second half
+    Node* head2 = slow->next; 
+        
+    // Make second half circular
+    fast->next = slow->next; 
+        
+    // Make first half circular
+    slow->next = head; 
+    
+    return {head1, head2};
+} 
+
+void printList(Node *head) { 
+    Node *curr = head; 
+    if(head != nullptr) { 
+        do { 
+        cout << curr->data << " "; 
+        curr = curr->next; 
+        } while(curr != head); 
+      	cout << endl; 
+    } 
+} 
+
+int main() { 
+    
+    Node *head = new Node(1); 
+    Node *head1 = nullptr; 
+    Node *head2 = nullptr; 
+
+    // Created linked list will be 1->2->3->4
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = head;
+    
+    pair<Node*, Node*> result = splitList(head); 
+    head1 = result.first;
+    head2 = result.second;
+    
+    printList(head1); 
+    printList(head2);
+    
+    return 0; 
+}
